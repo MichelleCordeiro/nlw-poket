d4fb534ac61ba7d0d9d563df9c4cc80f091f7198
Michelle Cordeiro <mic321@gmail.com>
2025-01-13 21:19:48 -0300

    feat: add market router in markets

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Alert, Text, View } from 'react-native'
 import MapView, { Callout, Marker } from 'react-native-maps'
 import * as Location from 'expo-location'
+import { router } from 'expo-router'
 
 import { api } from '@/services/api'
 import { fontFamily, colors } from '@/styles/theme'
@@ -21,55 +22,55 @@ const currentLocation = {
 }
 
 export default function Home() {
-  const [categories, setCategories] = useState<CategoriesProps>([]);
-  const [category, setCategory] = useState('');
-  const [markets, setMarkets] = useState<MarketsProps[]>([]);
+  const [categories, setCategories] = useState<CategoriesProps>([])
+  const [category, setCategory] = useState('')
+  const [markets, setMarkets] = useState<MarketsProps[]>([])
 
   async function fetchCategories() {
     try {
-      const { data } = await api.get('/categories');
-      setCategories(data);
-      setCategory(data[0].id);
+      const { data } = await api.get('/categories')
+      setCategories(data)
+      setCategory(data[0].id)
     } catch (error) {
-      console.log(error);
-      Alert.alert('Categorias', 'Não foi possível carregar as categorias.');
+      console.log(error)
+      Alert.alert('Categorias', 'Não foi possível carregar as categorias.')
     }
   }
 
   async function fetchMarkets() {
     try {
       if (!category) {
-        return;
+        return
       }
 
-      const { data } = await api.get('/markets/category/' + category);
-      setMarkets(data);
+      const { data } = await api.get('/markets/category/' + category)
+      setMarkets(data)
     } catch (error) {
-      console.log(error);
-      Alert.alert('Locais', 'Não foi possível carregar os locais.');
+      console.log(error)
+      Alert.alert('Locais', 'Não foi possível carregar os locais.')
     }
   }
 
   async function getCurrentLocation() {
     try {
-      const { granted } = await Location.requestForegroundPermissionsAsync();
+      const { granted } = await Location.requestForegroundPermissionsAsync()
 
       if (granted) {
-        const location = await Location.getCurrentPositionAsync();
+        const location = await Location.getCurrentPositionAsync()
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
   useEffect(() => {
     // getCurrentLocation()
-    fetchCategories();
-  }, []);
+    fetchCategories()
+  }, [])
 
   useEffect(() => {
-    fetchMarkets();
-  }, [category]);
+    fetchMarkets()
+  }, [category])
 
   return (
     <View style={{ flex: 1, backgroundColor: '#CECECE' }}>
@@ -103,7 +104,7 @@ export default function Home() {
             }}
             image={require('@/assets/pin.png')}
           >
-            <Callout>
+            <Callout onPress={() => router.navigate(`/market/${item.id}`)}>
               <View>
                 <Text
                   style={{
@@ -132,5 +133,5 @@ export default function Home() {
 
       <Places data={markets} />
     </View>
-  );
+  )
 }
