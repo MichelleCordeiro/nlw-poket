b0d454dee02e1e0c97b200bcbbff6c925e84364c
Michelle Cordeiro <mic321@gmail.com>
2025-01-13 22:32:38 -0300

    feat: add cover, details, info and coupon market components

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react'
-import { Alert, View } from 'react-native'
-import { router, useLocalSearchParams } from 'expo-router'
+import { Alert, View, Modal } from 'react-native'
+import { router, useLocalSearchParams, Redirect } from 'expo-router'
 
 import { Loading } from '@/components/loading'
+import { Cover } from '@/components/market/cover'
+import { Coupon } from '@/components/market/coupon'
+import { Details, PropsDetails } from '@/components/market/details'
+import { Button } from '@/components/button'
 
 import { api } from '@/services/api'
 
+type DataProps = PropsDetails & {
+  cover: string
+}
+
 export default function Market() {
-  const [data, setData] = useState()
+  const [data, setData] = useState<DataProps>()
+  const [coupon, setCoupon] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState(true)
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false)
 
   const params = useLocalSearchParams<{ id: string}>()
 
@@ -28,6 +38,15 @@ export default function Market() {
     }
   }
 
+  async function handleOpenCamera() {
+    try {
+      setIsVisibleCameraModal(true)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Câmera', 'Não foi possível utilizar a câmera')
+    }
+  }
+
   useEffect(() => {
     fetchMarket()
   },[params.id])
@@ -36,9 +55,29 @@ export default function Market() {
     return <Loading />
   }
 
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+  if(!data) {
+    return <Redirect href='/home' />
+  }
 
+  return (
+    <View style={{ flex: 1 }}>
+      <Cover uri={data.cover} />
+      <Details data={data} />
+      {coupon && <Coupon code={coupon} />}
+
+      <View style={{ padding: 32 }}>
+        <Button onPress={handleOpenCamera}>
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <View style={{ position: 'absolute', bottom: 32, left: 32, right: 32 }}>
+        <Button onPress={() => setIsVisibleCameraModal(false)}>
+          <Button.Title>Voltar</Button.Title>
+        </Button>
+        </View>
+      </Modal>
     </View>
   )
 }
